e09d45c63bcb182407b45db238044362dbce4236
Kanya26 <56309069+Kanya26@users.noreply.github.com>
2019-11-05 01:07:31 +0700

    Add files via upload

---
+++ price.py
@@ -0,0 +1,13 @@
+"""How much does it cost?"""
+def main():
+    """cost"""
+    name = input("name :")
+    name = name.split(" ")
+    food = input("food's name :")
+    food = food.split(" ")
+    pricefood = int(input("price's food :"))
+    pricefood = picefood.split(" ") 
+    print(name)
+    print(food)
+    print(pricefood)
+main()
